aae5af75228adb2d0ce3979e00b49fe50912e105
Carlos Aurelio <carlos.aurelio@tron.com.br>
2020-05-07 17:49:11 -0300

    #90152 - Adicionar empresas na Fila

--- TronBox.Application/Services/ConfiguracaoEmpresaAppService.cs
+++ TronBox.Application/Services/ConfiguracaoEmpresaAppService.cs
@@ -11,6 +11,7 @@ using Comum.DTO.Usuario;
 using FluentValidation.Results;
 using Newtonsoft.Json;
 using Sentinela.Domain.DTO;
+using Sentinela.Domain.Interfaces;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -19,6 +20,7 @@ using TronBox.Application.Services.Interfaces;
 using TronBox.Domain.Aggregates.ConfiguracaoEmpresaAgg;
 using TronBox.Domain.Aggregates.ConfiguracaoEmpresaAgg.Repository;
 using TronBox.Domain.DTO;
+using TronBox.Domain.Enums;
 using TronCore.DefinicoesConfiguracoes;
 using TronCore.Dominio.Base;
 using TronCore.Dominio.Bus;
@@ -31,6 +33,8 @@ namespace TronBox.Application.Services
 {
     public class ConfiguracaoEmpresaAppService : IConfiguracaoEmpresaAppService
     {
+        public static string URL_FILA_EMPRESA = "http://10.20.30.28:7000";
+
         #region Membros
         private readonly IBus _bus;
         private readonly IMapper _mapper;
@@ -103,6 +107,7 @@ namespace TronBox.Application.Services
                 _repositoryFactory.Instancie<IEmpresaRepository>().Atualizar(empresa);
 
                 ExcluirUsuarioCriarNovo(empresaExistente, empresa.EmailPrincipal);
+                InserirEmpresaFila(empresa, configuracaoEmpresa);
             }
         }
 
@@ -122,9 +127,11 @@ namespace TronBox.Application.Services
             if (resposta.Sucesso && certificadoCreateDTO.ManifestarAutomaticamente)
             {
                 var configuracaoEmpresa = BuscarConfiguracaoEmpresa();
-                
+
                 configuracaoEmpresa.ManifestarAutomaticamente = true;
                 _repositoryFactory.Instancie<IConfiguracaoEmpresaRepository>().Atualizar(configuracaoEmpresa);
+
+                InserirEmpresaFila(configuracaoEmpresa);
             }
 
             return resposta;
@@ -150,6 +157,14 @@ namespace TronBox.Application.Services
         {
             var empresa = _mapper.Map<EmpresaDTO>(_repositoryFactory.Instancie<IEmpresaRepository>().BuscarTodos().FirstOrDefault());
 
+            return ObterSituacaoCertificadoEmpresa(empresa);
+        }
+
+        #region Private Methods
+        private ConfiguracaoEmpresa BuscarConfiguracaoEmpresa() => _repositoryFactory.Instancie<IConfiguracaoEmpresaRepository>().BuscarTodos().FirstOrDefault();
+
+        private CertificadoSituacaoDTO ObterSituacaoCertificadoEmpresa(EmpresaDTO empresa)
+        {
             var certificadoResposta = UtilitarioHttpClient.GetRequest(_usuarioLogado.GetToken(), Constantes.URI_BASE_CT,
                 $"api/v1/certificados/{empresa.Inscricao}").GetAwaiter().GetResult();
 
@@ -166,8 +181,94 @@ namespace TronBox.Application.Services
             };
         }
 
-        #region Private Methods
-        private ConfiguracaoEmpresa BuscarConfiguracaoEmpresa() => _repositoryFactory.Instancie<IConfiguracaoEmpresaRepository>().BuscarTodos().FirstOrDefault();
+        private void InserirEmpresaFila(Empresa empresa, ConfiguracaoEmpresa configuracaoEmpresa)
+        {
+            if (configuracaoEmpresa.InscricoesComplementares.Any())
+            {
+                var tenantId = FabricaGeral.Instancie<ITenantProvider>().GetTenant().Id;
+
+                foreach (var inscricaoComplementar in configuracaoEmpresa.InscricoesComplementares)
+                {
+                    if (configuracaoEmpresa.DadosMatoGrosso != null && inscricaoComplementar.ConsultaMatoGrosso && inscricaoComplementar.Situacao == eSituacao.Ativo && !string.IsNullOrEmpty(inscricaoComplementar.InscricaoEstadual))
+                        AdicionarFilaMatoGrosso(empresa, configuracaoEmpresa, inscricaoComplementar, tenantId);
+                }
+            }
+        }
+
+        private void InserirEmpresaFila(ConfiguracaoEmpresa configuracaoEmpresa)
+        {
+            if (configuracaoEmpresa.InscricoesComplementares.Any())
+            {
+                var empresa = _repositoryFactory.Instancie<IEmpresaRepository>().BuscarTodos().FirstOrDefault();
+
+                var certificado = ObterSituacaoCertificadoEmpresa(_mapper.Map<EmpresaDTO>(empresa));
+
+                if (certificado != null && !certificado.Vencido)
+                {
+                    var tenantId = FabricaGeral.Instancie<ITenantProvider>().GetTenant().Id;
+
+                    foreach (var inscricaoComplementar in configuracaoEmpresa.InscricoesComplementares)
+                        AdicionarFilaNfe(empresa, configuracaoEmpresa, inscricaoComplementar, tenantId);
+                }
+            }
+        }
+
+        private static void AdicionarFilaNfe(Empresa empresa, ConfiguracaoEmpresa configuracaoEmpresa, InscricaoComplementar inscricaoComplementar, Guid tenantId)
+        {
+            var ultimoNsu = FabricaGeral.Instancie<IHistoricoConsultaAppService>().ObterUltimoNSU(ETipoDocumentoConsulta.NFe);
+
+            var empresaFila = new
+            {
+                id = tenantId,
+                dataCriacao = DateTime.Now,
+                nome = inscricaoComplementar.NomeFantasia ?? empresa.RazaoSocial,
+                inscricaoEmpresa = empresa.Inscricao,
+                uf = inscricaoComplementar.UF,
+                ultimoNsu,
+                configuracaoEmpresa = new
+                {
+                    manifestarAutomaticamente = configuracaoEmpresa.ManifestarAutomaticamente,
+                    salvarSomenteManifestadas = configuracaoEmpresa.SalvarSomenteManifestadas,
+                    metodoBusca = configuracaoEmpresa.MetodoBusca,
+                }
+            };
+
+            UtilitarioHttpClient.PostRequest(string.Empty, URL_FILA_EMPRESA, "api/nfes", empresaFila);
+        }
+
+        private static void AdicionarFilaMatoGrosso(Empresa empresa, ConfiguracaoEmpresa configuracaoEmpresa, InscricaoComplementar inscricaoComplementar, Guid tenantId)
+        {
+            var inscricaoEstadual = inscricaoComplementar.InscricaoEstadual.PadLeft(11, '0');
+
+            var dataFinalConsultada = FabricaGeral.Instancie<IHistoricoConsultaMatoGrossoAppService>().ObterUltimoPeriodo(inscricaoEstadual);
+
+            var dataInicial = dataFinalConsultada != null && dataFinalConsultada.HasValue
+                ? UtilitarioDatas.ConvertToIntDate(dataFinalConsultada.Value.AddDays(-7))
+                : configuracaoEmpresa.MetodoBusca == EMetodoBusca.MesAtual
+                    ? UtilitarioDatas.ConvertToIntDate(DateTime.Now.AddMonths(-1))
+                    : UtilitarioDatas.ConvertToIntDate(DateTime.Now.AddMonths(-3));
+
+            var dataFinal = UtilitarioDatas.ConvertToIntDate(DateTime.Now.AddDays(-1));
+
+            var empresaFila = new
+            {
+                id = tenantId,
+                dataCriacao = DateTime.Now,
+                nome = inscricaoComplementar.NomeFantasia ?? empresa.RazaoSocial,
+                inscricaoEmpresa = empresa.Inscricao,
+                inscricaoEstadual,
+                dadosMatoGrosso = new
+                {
+                    tipo = configuracaoEmpresa.DadosMatoGrosso.Tipo,
+                    usuario = configuracaoEmpresa.DadosMatoGrosso.Usuario,
+                    senha = configuracaoEmpresa.DadosMatoGrosso.Senha
+                },
+                dataInicial,
+                dataFinal,
+            };
+
+            UtilitarioHttpClient.PostRequest(string.Empty, URL_FILA_EMPRESA, "api/mato-grosso", empresaFila);
+        }
 
         private void ExcluirUsuarioCriarNovo(Empresa empresa, string novoEmail)
         {
