8bf6d8f97da55df06ad03d0ead4039a6688e1117
Rodrigo Oliveira <rdgsec@gmail.com>
2020-12-08 14:22:29 -0300

    Adiciona endpoint para buscar os totais de documentos armazenados em um periodo

--- TronBox.Application/Services/Interfaces/IDocumentoFiscalAppService.cs
+++ TronBox.Application/Services/Interfaces/IDocumentoFiscalAppService.cs
@@ -20,5 +20,6 @@ namespace TronBox.Application.Services.Interfaces
         void ConfirmarImportacao(IEnumerable<DadosImportacaoDTO> dadosImportacoes);
         void CancelarDocumentos(IEnumerable<string> chavesCancelamento);
         void BuscarManualmente(ETipoDocumentoConsulta tipo, DadosBuscaDTO dadosBuscaDTO);
+        TotalDocumentosArmazenadosDTO ObterTotalDocumentosArmazenados(int periodoInicial, int periodoFinal);
     }
 }
